a0d40fa9d75d75449d47071b0f4e902bfe87c9f1
Raquel De Biase <raqueldebiase@gmail.com>
2024-03-01 10:33:04 +0000

    fix bug slide

--- src/app/slide/slide.component.ts
+++ src/app/slide/slide.component.ts
@@ -13,7 +13,13 @@ export class SlideComponent implements AfterViewInit {
   items =  [
     {link1: 'https://www.google.com/', link2:'https://g1.globo.com/', imageUrl: 'assets/images/projeto1.png', altText:'profile'},
     {link: 'https://www.google.com/', imageUrl: 'assets/images/projeto2.png', altText:'profile'},
-    // ... (outras entradas)
+    {link: 'https://www.google.com/', imageUrl: 'assets/images/projeto3.png', altText:'profile'},
+    {link: 'https://www.google.com/', imageUrl: 'assets/images/projeto4.png', altText:'profile'},
+    {link: 'https://www.google.com/', imageUrl: 'assets/images/projeto5.png', altText:'profile'},
+    {link: 'https://www.google.com/', imageUrl: 'assets/images/projeto6.png', altText:'profile'},
+    {link: 'https://www.google.com/', imageUrl: 'assets/images/projeto7.png', altText:'profile'},
+    {link: 'https://www.google.com/', imageUrl: 'assets/images/projeto8.png', altText:'profile'},
+    {link: 'https://www.google.com/', imageUrl: 'assets/images/projeto9.png', altText:'profile'},
   ];
 
   isHovered: boolean[] = new Array(this.items.length).fill(false);
@@ -26,7 +32,7 @@ export class SlideComponent implements AfterViewInit {
     setTimeout(() => {
       const swiper = new Swiper('.swiper-container', {
         slidesPerView: 10,
-        spaceBetween: 0.5,
+        spaceBetween: 10,
         allowTouchMove: false,
         navigation: {
           nextEl: '.swiper-button-next',
@@ -47,15 +53,31 @@ export class SlideComponent implements AfterViewInit {
         breakpoints: {
           320: {
             slidesPerView: 1,
-            spaceBetween: 5,
+            spaceBetween: 300,
           },
           480: {
-            slidesPerView: 3,
-            spaceBetween: 30
+            slidesPerView: 1,
+            spaceBetween: 300
           },
           640: {
-            slidesPerView: 4,
-            spaceBetween: 40
+            slidesPerView: 3,
+            spaceBetween: 300
+          },
+          900: {
+            slidesPerView: 2,
+            spaceBetween: 400
+          },
+          1100: {
+            slidesPerView: 2,
+            spaceBetween: 200
+          },
+          1200: {
+            slidesPerView: 3,
+            spaceBetween: 500
+          },
+          1400: {
+            slidesPerView: 3,
+            spaceBetween: 300
           }
         },
         loop: true
