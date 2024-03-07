d87c7135281f4d4e14ea8fd362f04d897a44f0cc
Raquel De Biase <raqueldebiase@gmail.com>
2024-03-07 17:51:09 +0000

    firebase form

---
+++ src/app/firebase-config.js
@@ -0,0 +1,19 @@
+// firebase-config.js
+import { initializeApp } from "firebase/app";
+import "firebase/database";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBAwiIez1R14P1aCc7DowMLrzT52Kv8pyQ",
+  authDomain: "formulario-de-contato-21184.firebaseapp.com",
+  projectId: "formulario-de-contato-21184",
+  storageBucket: "formulario-de-contato-21184.appspot.com",
+  messagingSenderId: "565842643351",
+  appId: "1:565842643351:web:013235b21df1b7571a5cd2",
+  measurementId: "G-T11DEGMCDT",
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export default firebaseConfig;
--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,36 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Subject } from 'rxjs';
 import { formulario } from '../interfaces/formulario';
 import { HttpService } from '../services/http.service';
 
+// Importe apenas os módulos necessários do Firebase
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, push } from 'firebase/database';
 
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
-  styleUrl: './formulario.component.css'
+  styleUrls: ['./formulario.component.css']
 })
-
-//Criando o controlador de gerenciamento de estado do formulário
-export class FormularioComponent implements OnInit{
-
+export class FormularioComponent implements OnInit {
   public form!: FormGroup;
-
   mostrarMensagem: boolean = false;
 
   constructor(private formBuilder: FormBuilder, private http: HttpService) {}
-
-  //inicializando o formulário
 
   ngOnInit(): void {
     this.form = this.formBuilder.group({
       name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       interest: ['', Validators.required],
-      message:['', Validators.required]
+      message: ['', Validators.required]
     });
-  };
-  
+  }
+
   public onSubmit() {
     if (this.form.valid) {
       const form: formulario = {
@@ -38,11 +34,36 @@ export class FormularioComponent implements OnInit{
         email: this.form.controls['email'].value,
         interest: this.form.controls['interest'].value,
         message: this.form.controls['message'].value,
-      }
-      this.http.insert(form).subscribe(x=>{
+      };
+
+      this.http.insert(form).subscribe(x => {
         console.log(x);
-      })
+      });
+
       this.mostrarMensagem = true;
+
+      // Enviar dados para o Firebase
+      const firebaseConfig = {
+        apiKey: "AIzaSyBAwiIez1R14P1aCc7DowMLrzT52Kv8pyQ",
+        authDomain: "formulario-de-contato-21184.firebaseapp.com",
+        projectId: "formulario-de-contato-21184",
+        storageBucket: "formulario-de-contato-21184.appspot.com",
+        messagingSenderId: "565842643351",
+        appId: "1:565842643351:web:013235b21df1b7571a5cd2",
+        measurementId: "G-T11DEGMCDT",
+      };
+
+      // Inicialize o aplicativo Firebase
+      const firebaseApp = initializeApp(firebaseConfig);
+
+      // Obtenha uma referência ao banco de dados
+      const database = getDatabase(firebaseApp);
+
+      // Caminho no banco de dados
+      const caminhoNoBancoDeDados = 'formulario-portfolio/QNHteGpjHD8hd5DaCWLO';
+
+      // Enviar dados para o Firebase
+      push(ref(database, caminhoNoBancoDeDados), form);
     }
   }
 }
