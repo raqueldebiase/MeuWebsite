01913b30c612ef1514251fbee17df1264ef0705b
Raquel De Biase <raqueldebiase@gmail.com>
2024-03-20 11:09:45 +0000

    add urls projetos fix link 2

--- src/app/slide/slide.component.ts
+++ src/app/slide/slide.component.ts
@@ -14,10 +14,10 @@ export class SlideComponent implements AfterViewInit {
     {link1: 'https://github.com/raqueldebiase/buondiforedit',link2:'https://buondiforedit360.netlify.app', imageUrl: 'assets/images/proj01.png', altText:'projeto buondi para edit'},
     {link1: 'https://github.com/raqueldebiase/Projeto-final-Edit-Ecommerce',link2:'https://projetofinaledit.netlify.app', imageUrl: 'assets/images/proj02.png', altText:'projeto final edit'},
     {link1: 'https://github.com/raqueldebiase/landscape',link2:'https://landingpageskincare.netlify.app', imageUrl: 'assets/images/proj03.png', altText:'landscape skincare'},
-    {link1: 'https://github.com/raqueldebiase/landscape-II',link2:'https://landingpagethisseason.netlify.app', imageUrl: 'assets/images/proj04.png', altText:'landscape season'},
-    {link: 'https://www.google.com/', imageUrl: 'assets/images/proj05.png', altText:'profile'},
+    {link1: '',link2:'https://barralhydra.pt/', imageUrl: 'assets/images/proj04.png', altText:'barral'},
+    {link1: 'https://www.google.com/', imageUrl: 'assets/images/proj05.png', altText:'profile'},
     {link: 'https://www.google.com/', imageUrl: 'assets/images/proj06.png', altText:'profile'},
-    {link: 'https://www.google.com/', imageUrl: 'assets/images/proj07.png', altText:'profile'},
+    {link1: 'https://github.com/raqueldebiase/landscape-II',link2:'https://landingpagethisseason.netlify.app', altText:'landscape season'},
     {link: 'https://www.google.com/', imageUrl: 'assets/images/proj08.png', altText:'profile'},
     {link: 'https://www.google.com/', imageUrl: 'assets/images/proj09.png', altText:'profile'},
   ];
