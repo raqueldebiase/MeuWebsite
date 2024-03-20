bff8746ed9c7ca43162c031eeefa379aea3e5954
Raquel De Biase <raqueldebiase@gmail.com>
2024-03-20 11:15:34 +0000

    add urls projetos fix link 3

--- src/app/slide/slide.component.ts
+++ src/app/slide/slide.component.ts
@@ -17,9 +17,8 @@ export class SlideComponent implements AfterViewInit {
     {link1: '',link2:'https://barralhydra.pt/', imageUrl: 'assets/images/proj04.png', altText:'barral'},
     {link1: 'https://www.google.com/', imageUrl: 'assets/images/proj05.png', altText:'profile'},
     {link: 'https://www.google.com/', imageUrl: 'assets/images/proj06.png', altText:'profile'},
-    {link1: 'https://github.com/raqueldebiase/landscape-II',link2:'https://landingpagethisseason.netlify.app', altText:'landscape season'},
-    {link: 'https://www.google.com/', imageUrl: 'assets/images/proj08.png', altText:'profile'},
-    {link: 'https://www.google.com/', imageUrl: 'assets/images/proj09.png', altText:'profile'},
+    {link1: 'https://github.com/raqueldebiase/landscape-II',link2:'https://landingpagethisseason.netlify.app',imageUrl: 'assets/images/proj07.png', altText:'landscape season'},
+    {link1: 'https://github.com/raqueldebiase/grid-js', link2:'https://landingpagegridcss.netlify.app', imageUrl: 'assets/images/proj09.png', altText:'profile'},
   ];
 
   isHovered: boolean[] = new Array(this.items.length).fill(false);
