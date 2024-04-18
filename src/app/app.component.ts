498d49a349c7c9f997319e09b6e2d7037a3d2fab
Raquel De Biase <raqueldebiase@gmail.com>
2024-04-18 17:28:13 +0100

    fix parallax mobile teste 13

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,10 +14,10 @@ export class AppComponent {
     const isMobile = window.innerWidth <= 767; // Defina o limite de largura para dispositivos móveis conforme necessário
 
     if (isMobile) {
-      this.parallaxOffset = window.pageYOffset * 0.6; // Ajuste o valor para dispositivos móveis
+      this.parallaxOffset = window.pageYOffset * 0.9; // Ajuste o valor para dispositivos móveis
       this.parallax2Offset = window.pageYOffset * 0.1; // Ajuste o valor para dispositivos móveis
     } else {
-      this.parallaxOffset = window.pageYOffset * 0.4; // Valor original para desktop
+      this.parallaxOffset = window.pageYOffset * 0.6; // Valor original para desktop
       this.parallax2Offset = window.pageYOffset * 0.1; // Valor original para desktop
     }
   }
