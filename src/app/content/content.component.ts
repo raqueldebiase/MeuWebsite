00b2bde3aee9c5c29a2e5fc192ed9f71758426fc
Raquel De Biase <raqueldebiase@gmail.com>
2024-04-18 14:08:13 +0100

    add parallax

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
-  styleUrl: './content.component.css'
+  styleUrls: ['./content.component.css']
 })
 export class ContentComponent {
+  parallaxOffset: number = 0;
+  parallax2Offset: number = 0;
 
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(event: any) {
+    this.parallaxOffset = window.pageYOffset * 0.7; // Ajuste o valor (0.7) para a velocidade desejada do parallax
+    this.parallax2Offset = window.pageYOffset * 0.1; // Ajuste o valor (0.5) para a velocidade desejada do segundo parallax
+  }
 }
