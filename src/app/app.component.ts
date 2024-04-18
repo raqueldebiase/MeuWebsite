950229aa8dcb0a62ccb891c52fd115bf2f460124
Raquel De Biase <raqueldebiase@gmail.com>
2024-04-18 15:57:57 +0100

    fix parallax mobile teste 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit, HostListener } from '@angular/core';
 
-
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -15,21 +13,25 @@ export class AppComponent implements OnInit{
   interest = '';
   message= '';
 
-  constructor(){
+  constructor(){}
 
-  }
-
-  ngOnInit(): void {
-    
-  }
+  ngOnInit(): void {}
 
   parallaxOffset: number = 0;
   parallax2Offset: number = 0;
 
   @HostListener('window:scroll', ['$event'])
   onWindowScroll(event: any) {
-    this.parallaxOffset = window.pageYOffset * 0.6; // Ajuste o valor (0.7) para a velocidade desejada do parallax
-    this.parallax2Offset = window.pageYOffset * 0.1; // Ajuste o valor (0.5) para a velocidade desejada do segundo parallax
-  }
+    // Verifica se o dispositivo é um dispositivo móvel
+    const isMobile = window.innerWidth <= 768; // Você pode ajustar este valor conforme necessário
 
+    // Ajusta os valores de parallax de acordo com o tipo de dispositivo
+    if (isMobile) {
+      this.parallaxOffset = window.pageYOffset * 0.3; // Ajuste o valor para uma velocidade mais lenta no parallax
+      this.parallax2Offset = window.pageYOffset * 0.05; // Ajuste o valor para uma velocidade mais lenta no segundo parallax
+    } else {
+      this.parallaxOffset = window.pageYOffset * 0.6; // Valor original para dispositivos desktop
+      this.parallax2Offset = window.pageYOffset * 0.1; // Valor original para dispositivos desktop
+    }
+  }
 }
